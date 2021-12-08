187b79bffbfb9150850be53967114091dc35152a
Mike Stmria <coderstitch@gmail.com>
2021-12-07 22:55:07 -0600

    Added Image to readme. Added more code to js for functionality on saving to local storage

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # proyect-manager-calendar
 This is a web app calendar to help you organize your schedule. It is done using html5, Bootstrap for styling and Javascript. 
 
+
+## Demo Image
+https://imgur.com/a/O7tULSV
+
 ## Link to Application
 https://stitchcodes.github.io/proyect-manager-calendar/
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,33 @@ for (i = 9; i < 17; i++) {
 
     var section = document.createElement("section");
     $("section").addClass("hour row");
-    section.innerHTML = hourcreate + ":00";
+    section.innerHTML = "<p>" + hourcreate + ":00 </p>";
     
     var textarea = document.createElement("textarea");
-    $("textarea").addClass("past");
+    $("textarea").attr("data-box", hourcreate);
+    $("textarea").addClass("flex-fill past");
 
     var button = document.createElement("button");
+    $("button").attr("data-save", hourcreate)
     button.innerHTML = "&#x23FA";
 
     container.append(section);
     section.appendChild(textarea);
     section.appendChild(button);
+    button.addEventListener("click", storeTask);
+}
+
+// STORE TASK FUNCTION
+function storeTask (event) {
+    var saveBtnClick = event.target;
+    console.log("You clicked: ",saveBtnClick);
+
+    var test = saveBtnClick
+    console.log(test);
+    // IF EVENT CLICK SAVE EVENT BOX DATA
+    // $(this).find(':selected').data('box');
+
+    localStorage.getItem("task", "text from box");
 }
 
 // Display Current Time
