6f0ec76b901cf2bb208e1c583dac3098a8095bd2
StitchCodes <coderstitch@gmail.com>
2021-12-07 22:07:04 -0600

    Added buttons and textarea. Starting work on save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,20 @@ var hours24 = []
 for (i = 9; i < 17; i++) {
     var hourcreate = [i];
     hours24.push(hourcreate++);
+
     var section = document.createElement("section");
     $("section").addClass("hour row");
     section.innerHTML = hourcreate + ":00";
+    
+    var textarea = document.createElement("textarea");
+    $("textarea").addClass("past");
+
+    var button = document.createElement("button");
+    button.innerHTML = "&#x23FA";
+
     container.append(section);
+    section.appendChild(textarea);
+    section.appendChild(button);
 }
 
 // Display Current Time
